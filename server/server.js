d11a3ece5b285325d4ebfecd042d4ba9607d5c93
Pradeepa K R <pradeepakr2006email@gmail.com>
2025-12-31 11:20:34 +0530

    Fixed Route Error

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ app.post('/api/contact', async (req, res) => {
 });
 
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
